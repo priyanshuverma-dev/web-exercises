020c148ee3345c018fb4543ba93ba19a762662b6
Antriksh Verma <antrikshdevs@gmail.com>
2023-05-25 11:20:35 +0530

    slug changed

---
+++ music/js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Dewaane Hum Nahi",
+    artist: "Aditya Yadav",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Ik Tu Hai - Attack",
+    artist: "Shashwat Sachdev",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Kesariya - Brahmastra",
+    artist: "Arijit Singh",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Piya O Re Piya Video Song - Tere Naal Love Ho Gaya",
+    artist: "Riteish Deshmukh, Genelia Dsouza",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Gazab Ka Hai Din",
+    artist: "Aamir Khan, Juhi Chawla",
+    img: "music-5",
+    src: "music-6",
+  },
+  {
+    name: "Soulmate",
+    artist: "Akull, Aastha Gill",
+    img: "music-6",
+    src: "music-5",
+  },
+];
